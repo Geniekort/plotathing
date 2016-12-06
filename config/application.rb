d8bd22e6d962c9694089a74489f0b7fc26e2431a
Stufingo <geniekort@hotmail.com>
2016-12-06 13:01:14 +0100

    heroku push

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,12 @@ module Plotathing
 	#to prevent some bug with bootstrap precompiling
 	#config.assets.initialize_on_precompile = true
 	config.assets.precompile += %w( alert.js )
-	config.assets.digest  = true
+	# Disable Rails's static asset server (Apache or nginx will already do this)
+	config.serve_static_assets = false
+	# Don't fallback to assets pipeline if a precompiled asset is missed
+	config.assets.compile = false
+	# Generate digests for assets URLs
+	config.assets.digest = true
   end
 end
 
